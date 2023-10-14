388512afe330f6ee1cc2dc06e1067d5aa9e2bceb
madsonyung <arthurrocha2101@gmail.com>
2023-10-14 07:52:34 -0300

    adicionados arquivos do dom

--- teste_dom-main/script.js
+++ teste_dom-main/script.js
@@ -50,23 +50,3 @@ uma chamado .Hidden (tenha display: none;)
 outra chamada .Show (tenha display: block;)
 
 */
-
-function  clickar() {
-    //validar se tem hidden
-
-    const footer = document.querySelector("footer");
-    const btn = document.querySelector("button");
-
-    if (footer.className === "hidden") {
-
-        footer.className = "show";
-        btn.innerHTML = "Esconder footer";
-        
-    } else {
-
-        footer.className = "hidden";
-        btn.innerHTML = "Mostrar footer";
-    }
-}
-
-
